a20c148a72dbe3bac49a50481e6e8aba7c86e531
Magarec <magarec81@gmail.com>
2026-04-28 17:35:05 +0200

    Adjust splash duration

--- src/App.jsx
+++ src/App.jsx
@@ -283,7 +283,7 @@ export default function App() {
   useEffect(() => {
   const timer = setTimeout(() => {
     setShowSplash(false);
-  }, 1800);
+  }, 5000);
 
   return () => clearTimeout(timer);
 }, []);
